bc2aa3711db3274214ac41e55c6a82ae83766994
kimbogeun <intulda@naver.com>
2021-10-31 20:25:51 +0900

    read a book for a month : [refactoring] 맛보기 예제 1 - amount 계산 메소드로 분리 오리지날 패키지 추가 * Customer * Movie * Rental
    리팩토링용 패키지 추가
    * Customer
    * Movie
    * Rental

---
+++ Refactoring/example/page24/original/Rental.java
@@ -0,0 +1,20 @@
+package example.page24.original;
+
+public class Rental {
+
+    private Movie _movie;
+    private int _dayRented;
+
+    public Rental(Movie movie, int dayRented) {
+        _movie = movie;
+        _dayRented = dayRented;
+    }
+
+    public int getDaysRented() {
+        return _dayRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+}
