8fed5605c77bb563a3919d5d0f06b48f1f226fe2
kimbogeun <intulda@naver.com>
2021-11-11 17:55:02 +0900

    refactor : page24 맛보기 예제 리팩토링 완료

--- Refactoring/example/page24/refactor/Movie.java
+++ Refactoring/example/page24/refactor/Movie.java
@@ -6,19 +6,30 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
-    public void setPriceCode(int priceCode) {
-        this._priceCode = priceCode;
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrenPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+            default:
+                throw new IllegalArgumentException("가격 코드가 잘못됐습니다.");
+        }
     }
 
     public String getTitle() {
@@ -27,32 +38,10 @@ public class Movie {
 
     // 비디오 종류별 대여료 계산 기능을 빼내어 별도의 함수로 작성
     double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                    break;
-                }
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
