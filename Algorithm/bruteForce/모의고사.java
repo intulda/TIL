9a8f2f14a6877fec75f86695a65dd1cdbce4c5cc
김보근 <intulda@naver.com>
2026-02-26 17:28:17 +0900

    알고리즘: 모의고사

---
+++ Algorithm/bruteForce/모의고사.java
@@ -0,0 +1,38 @@
+package bruteForce;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 모의고사 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[] {1,2,3,4,5}))); // [1]
+        System.out.println(Arrays.toString(solution(new int[] {1,3,2,4,2}))); // [1, 2, 3]
+        System.out.println(Arrays.toString(solution(new int[] {1,4,5,2,3,4,4,1,2,3,4,5,1,2}))); // [2, 3]
+    }
+
+    static int[] solution(int[] answers) {
+        int[] giveUp1 = {1, 2, 3, 4, 5};
+        int[] giveUp2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] giveUp3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = {0, 0, 0};
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == giveUp1[i % giveUp1.length]) score[0]++;
+            if (answers[i] == giveUp2[i % giveUp2.length]) score[1]++;
+            if (answers[i] == giveUp3[i % giveUp3.length]) score[2]++;
+        }
+
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            if (score[i] == max) {
+                list.add(i + 1);
+            }
+        }
+
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
