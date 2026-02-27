234f42a123ebd433eb3843cbe77cd57037b2f4b6
김보근 <intulda@naver.com>
2026-02-27 12:20:27 +0900

    알고리즘: 기능개발 지우고 다시 개발 - 위에 주석이 있음에도 불과하고 다시 수학으로 계산하는 방법을 떠올리지 못함

--- Algorithm/stack_queue/기능개발.java
+++ Algorithm/stack_queue/기능개발.java
@@ -1,14 +1,13 @@
 package stack_queue;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class 기능개발 {
     public static void main(String[] args) {
-        int[] progresses = {100, 100, 100};
-        int[] speeds = {1, 1, 1};
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
 
-        System.out.println(Arrays.toString(Solution.solution(progresses, speeds)));
+        System.out.println(Arrays.toString(solution(progresses, speeds)));
     }
 
     /**
@@ -17,71 +16,45 @@ public class 기능개발 {
      * 이 부분이 제대로 이해가 가지 않았는데, 딱 나누어 떨어지지않으면 하루가 더 필요한 상황이 오기때문에 그렇다고 설명받았다.
      * 자그만한 수학 공식을 외우는 것도 중요한 것 같다..
      */
-    public class Solution {
-        public static int[] solution(int[] progresses, int[] speeds) {
-            /**
-             * ArrayList<Integer> list = new ArrayList<>();
-             *         ArrayList<Integer> list2 = new ArrayList<>();
-             *         ArrayList<Integer> result = new ArrayList<>();
-             *
-             *             for (int j : progresses) {
-             *                 int remain_progress = 100 - j;
-             *                 list.add(remain_progress);
-             *             }
-             *
-             *             for (int i = 0; i < speeds.length; i++) {
-             *                 int currentDay = 0;
-             *
-             *                 if (list.get(i) == 0) {
-             *                     continue;
-             *                 }
-             *
-             *                 for (int j = speeds[i]; j <= list.get(i); j += speeds[i]) {
-             *                     currentDay++;
-             *                 }
-             *
-             *
-             *                 if (!list2.isEmpty() && list2.get(list2.size() - 1) >= currentDay) {
-             *                     int last = result.get(result.size() - 1);
-             *                     result.set(result.size() - 1, ++last);
-             *                     continue;
-             *                 }
-             *
-             *                 if (list2.isEmpty() || list2.get(list2.size() - 1) < currentDay) {
-             *                     list2.add(currentDay);
-             *                     result.add(1);
-             *                 }
-             *
-             *             }
-             *
-             *             return result.stream().mapToInt(Integer::intValue).toArray();
-             */
-            ArrayList<Integer> result = new ArrayList<>();
+    static int[] solution(int[] progresses, int[] speeds) {
+        int[] answers = {};
 
-            int[] days = new int[progresses.length];
-            for (int i = 0; i < progresses.length; i++) {
-                int remainProgress = 100 - progresses[i];
-                if (remainProgress <= 0) {
-                    days[i] = 0;
+        // 작업 진행도에서 각 스피드를 체크해 몇일 뒤 배포가 가능한건지 배열로 리턴해야함
+        // 앞의 작업이 배포가 안되면 그만큼 뒤의 작업은 늘어난다
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        int index = 0;
+        int prevDay = 0;
+        int currentDay = 0;
+
+        while (index < progresses.length) {
+            currentDay++;
+            progresses[index] = progresses[index] + speeds[index];
+
+            if (progresses[index] >= 100) {
+                if (prevDay == 0) {
+                    prevDay = currentDay;
+                }
+
+                if (prevDay >= currentDay) {
+                    map.put(prevDay, map.getOrDefault(prevDay, 0) + 1);
                 } else {
-                    days[i] = (remainProgress + speeds[i] - 1) / speeds[i];
-                }
-            }
-
-            int i = 0;
-            while (i < days.length) {
-                int count = 1;
-                int currentMaxDay = days[i];
-
-                while (i + count < days.length && days[i + count] <= currentMaxDay) {
-                    count++;
+                    map.put(currentDay, map.getOrDefault(currentDay, 0) + 1);
+                    prevDay = currentDay;
                 }
 
-                result.add(count);
-                i += count;
+                currentDay = 0;
+                index++;
             }
-
-            return result.stream().mapToInt(Integer::intValue).toArray();
         }
+
+        answers = new int[map.size()];
+
+        int _index = 0;
+        for (int value : map.keySet()) {
+            answers[_index++] = map.get(value);
+        }
+
+        return answers;
     }
 }
